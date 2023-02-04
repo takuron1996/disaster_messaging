232711c9c8385861d4a008f1152b7365649ce478
taku ikegami <takuron1996@gmail.com>
2023-02-04 17:58:46 +0900

    ✨ requestの機能を追加

--- application/src/main.rs
+++ application/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::Client;
+
+mod request;
+
+#[tokio::main]
+async fn main() {
+    let request = request::Request {
+        client: Client::new(),
+    };
+    let url = "https://crisis.yahoo.co.jp/evacuation/";
+    let html = request.get_response(url).await.text().await.unwrap();
+    println!("{html}");
 }
